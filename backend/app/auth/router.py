14f3e70f333af55952d05c730274b821d4955e86
Joao Pedro <joaopioonze@gmail.com>
2026-07-09 19:41:07 -0300

    feat: adiciona funcionalidade de refresh token e melhora estrutura do codigo do dominio auth

--- backend/app/auth/router.py
+++ backend/app/auth/router.py
@@ -1,15 +1,31 @@
 from http import HTTPStatus
+import os
 
-from fastapi import APIRouter, Depends, HTTPException
-from sqlalchemy import select
+from dotenv import load_dotenv
+from fastapi import APIRouter, Cookie, Depends, Response
 from sqlalchemy.orm import Session
 
-from app.auth.models import Users
 from app.auth.schemas import PrivateUser, PublicUser, RequestLogin
-from app.auth.service import get_password_hash, verify_password
+from app.auth.service import (
+    add_user_in_db,
+    create_refresh_token,
+    decode_access_token,
+    decode_refresh_token,
+    delete_user_session,
+    set_cookies,
+    update_refresh_token,
+    validate_user_login_credentials,
+    validate_user_registration_credentials,
+    validate_user_session,
+)
 from app.core.database import get_db
 from app.core.security import create_token
 
+load_dotenv()
+
+ACCESS_TOKEN_EXPIRE_MINUTES = 15
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
 router = APIRouter(prefix="/auth", tags=["Auth"])
 
 
@@ -18,57 +34,74 @@ router = APIRouter(prefix="/auth", tags=["Auth"])
 )
 def user_registration(user: PrivateUser, session: Session = Depends(get_db)):
 
-    existing_user = session.scalar(select(Users).where(Users.email == user.email))
-
-    if existing_user.email == user.email:
-        raise HTTPException(
-            status_code=HTTPStatus.CONFLICT,
-            detail="Email already exists",
-        )
-
-    if user.password1 != user.password2:
-        raise HTTPException(
-            status_code=HTTPStatus.UNAUTHORIZED,
-            detail="The passwords are different.",
-        )
-
-    hashed_password = get_password_hash(user.password1)
-    new_user = Users(
-        username=user.username, email=user.email, password_hash=hashed_password
-    )
-
-    session.add(new_user)
-    session.commit()
-    session.refresh(new_user)
+    validate_user_registration_credentials(user, session)
+    add_user_in_db(user, session)
 
     return {
-        "username": new_user.username,
-        "email": new_user.email,
+        "username": user.username,
+        "email": user.email,
     }
 
 
 @router.post("/user_login/", status_code=HTTPStatus.OK)
-def user_login(data: RequestLogin, session: Session = Depends(get_db)):
+def user_login(
+    data: RequestLogin, response: Response, session: Session = Depends(get_db)
+):
 
-    existing_user = session.scalar(select(Users).where(Users.email == data.email))
+    user_validated = validate_user_login_credentials(data, session)
 
-    if existing_user is None:
-        raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND,
-            detail="Email not found",
-        )
+    access_token = create_token(
+        data={"email": user_validated["email"], "id": user_validated["id"]}
+    )
+    refresh_token = create_refresh_token(data.email, session)
 
-    if not verify_password(data.password, existing_user.password_hash):
-        raise HTTPException(
-            status_code=HTTPStatus.UNAUTHORIZED,
-            detail="Incorrect password",
-        )
+    set_cookies(response, access_token, refresh_token)
+
+    """excluir session caso exista e criar outra"""
+
+    return {"Message": "Login successful"}
+
+
+@router.post("/refresh_token/", status_code=HTTPStatus.OK)
+def refresh_token(
+    response: Response,
+    session: Session = Depends(get_db),
+    access_token: str | None = Cookie(default=None),
+    refresh_token: str | None = Cookie(default=None),
+):
+
+    subject_id, subject_email = decode_access_token(access_token)
+    subject_key_hash = decode_refresh_token(refresh_token)
+
+    validate_user_session(subject_id, subject_key_hash, session, response)
 
     access_token = create_token(
         data={
-            "email": str(existing_user.email),
-            "id": str(existing_user.id),
+            "email": subject_email,
+            "id": subject_id,
         }
     )
+    refresh_token = update_refresh_token(subject_key_hash, subject_id, session)
 
-    return {"access_token": access_token, "token_type": "bearer"}
+    set_cookies(response, access_token, refresh_token)
+
+    return {"Message": "Token updated"}
+
+
+@router.delete("/user_logout/", status_code=HTTPStatus.OK)
+def logout_user(
+    response: Response,
+    session: Session = Depends(get_db),
+    access_token: str | None = Cookie(default=None),
+    refresh_token: str | None = Cookie(default=None),
+):
+
+    subject_id, subject_email = decode_access_token(access_token)
+    subject_key_hash = decode_refresh_token(refresh_token)
+
+    delete_user_session(subject_key_hash, subject_id, session)
+
+    response.delete_cookie("access_token")
+    response.delete_cookie("refresh_token")
+
+    return {"Message": "Logout successful"}
--- backend/app/auth/service.py
+++ backend/app/auth/service.py
@@ -1,5 +1,24 @@
-from pwdlib import PasswordHash
+from datetime import datetime
+import hashlib
+from http import HTTPStatus
+import os
+import secrets
 
+from dotenv import load_dotenv
+from fastapi import HTTPException, Response
+from jwt import decode
+from pwdlib import PasswordHash
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from app.auth.models import AuthSessions, Users
+from app.auth.schemas import PrivateUser, RequestLogin
+from app.core.security import create_token
+
+load_dotenv()
+ACCESS_TOKEN_EXPIRE_MINUTES = 15
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
 pwd_context = PasswordHash.recommended()
 
 
@@ -9,3 +28,196 @@ def get_password_hash(password: str):
 
 def verify_password(password: str, hashed_password: str):
     return pwd_context.verify(password, hashed_password)
+
+
+def validate_user_registration_credentials(user: PrivateUser, session: Session):
+
+    user_in_db = session.scalar(select(Users).where(Users.email == user.email))
+
+    if user_in_db:
+        if user_in_db.email == user.email:
+            raise HTTPException(
+                status_code=HTTPStatus.CONFLICT,
+                detail="Email already exists",
+            )
+
+        if user.password1 != user.password2:
+            raise HTTPException(
+                status_code=HTTPStatus.UNAUTHORIZED,
+                detail="The passwords are different.",
+            )
+
+
+def add_user_in_db(user: PrivateUser, session: Session):
+
+    hashed_password = get_password_hash(user.password1)
+    new_user = Users(
+        username=user.username, email=user.email, password_hash=hashed_password
+    )
+
+    session.add(new_user)
+    session.commit()
+    session.refresh(new_user)
+
+
+def validate_user_login_credentials(data: RequestLogin, session: Session):
+
+    user_in_db = session.scalar(select(Users).where(Users.email == data.email))
+
+    if user_in_db is None:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND,
+            detail="Email not found",
+        )
+
+    if not verify_password(data.password, user_in_db.password_hash):
+        raise HTTPException(
+            status_code=HTTPStatus.UNAUTHORIZED,
+            detail="Incorrect password",
+        )
+
+    return {
+        "id": user_in_db.id,
+        "username": user_in_db.username,
+        "email": user_in_db.email,
+    }
+
+
+def create_refresh_token(user_email, session: Session):
+
+    key = secrets.token_urlsafe(64)
+    key_hash = hashlib.sha256(key.encode()).hexdigest()
+
+    refresh_token = create_token(data={"key": key})
+
+    user_in_db = session.scalar(select(Users).where(Users.email == user_email))
+
+    new_session = AuthSessions(
+        user_id=user_in_db.id,
+        refresh_token_hash=key_hash,
+    )
+
+    session.add(new_session)
+    session.commit()
+    session.refresh(new_session)
+
+    return refresh_token
+
+
+def set_cookies(response: Response, access_token, refresh_token):
+
+    response.set_cookie(
+        key="access_token",
+        value=access_token,
+        httponly=True,
+        secure=True,
+        samesite="lax",
+        max_age=900,
+    )
+
+    response.set_cookie(
+        key="refresh_token",
+        value=refresh_token,
+        httponly=True,
+        secure=True,
+        samesite="lax",
+        max_age=604800,
+    )
+
+
+def decode_access_token(access_token):
+
+    if not access_token:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail="Access token not found"
+        )
+
+    payload = decode(access_token, SECRET_KEY, algorithms=[ALGORITHM])
+    subject_id = payload.get("id")
+    subject_email = payload.get("email")
+
+    return subject_id, subject_email
+
+
+def decode_refresh_token(refresh_token):
+
+    if not refresh_token:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail="Refresh token not found"
+        )
+
+    payload = decode(refresh_token, SECRET_KEY, algorithms=[ALGORITHM])
+
+    subject_key = payload.get("key")
+    subject_key_hash = hashlib.sha256(subject_key.encode()).hexdigest()
+
+    return subject_key_hash
+
+
+def validate_user_session(user_id, key_hash, session: Session, response: Response):
+
+    existing_session = session.scalar(
+        select(AuthSessions).where(
+            AuthSessions.refresh_token_hash == key_hash,
+            AuthSessions.user_id == int(user_id),
+        )
+    )
+
+    if existing_session:
+        if existing_session.revoked == True:
+            response.delete_cookie("access_token")
+            response.delete_cookie("refresh_token")
+
+            raise HTTPException(
+                status_code=HTTPStatus.UNAUTHORIZED, detail="Token revoked"
+            )
+
+        if existing_session.expires_at <= datetime.now():
+            response.delete_cookie("access_token")
+            response.delete_cookie("refresh_token")
+
+            raise HTTPException(
+                status_code=HTTPStatus.UNAUTHORIZED, detail="Token expired"
+            )
+    else:
+        response.delete_cookie("access_token")
+        response.delete_cookie("refresh_token")
+
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail="Refresh token not founddd"
+        )
+
+
+def update_refresh_token(key_hash, user_id, session):
+
+    new_key = secrets.token_urlsafe(64)
+    new_key_hash = hashlib.sha256(new_key.encode()).hexdigest()
+
+    refresh_token = create_token(data={"key": new_key})
+
+    session_in_db = session.scalar(
+        select(AuthSessions).where(
+            AuthSessions.refresh_token_hash == key_hash,
+            AuthSessions.user_id == int(user_id),
+        )
+    )
+
+    session_in_db.refresh_token_hash = new_key_hash
+
+    session.commit()
+    session.refresh(session_in_db)
+
+    return refresh_token
+
+
+def delete_user_session(key_hash, user_id, session: Session):
+
+    current_session = session.scalar(
+        select(AuthSessions).where(
+            AuthSessions.refresh_token_hash == key_hash,
+            AuthSessions.user_id == int(user_id),
+        )
+    )
+
+    session.delete(current_session)
+    session.commit()
