333c3e375a32a07913d80b3f121536a06f922595
Joao Pedro <joaopioonze@gmail.com>
2026-08-13 15:21:37 -0300

    feat: conecta pagina de registro de usuarios com o backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.auth.router import router as auth_router
 from app.conversation.router import router as conversation_router
 from app.document.router import router as document_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/test")
+def test():
+    return {"message": "API funcionando!"}
+
 app.include_router(auth_router)
 app.include_router(document_router)
 app.include_router(conversation_router)
--- frontend/src/features/auth/pages/registerPage.jsx
+++ frontend/src/features/auth/pages/registerPage.jsx
@@ -2,17 +2,90 @@ import './registerPage.css'
 import { Link } from 'react-router-dom'
 import { useState } from 'react'
 
+const upperWord = [
+  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 
+  'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 
+  'U', 'V', 'W', 'X', 'Y', 'Z'
+];
+
+const lowerWord = [
+  'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 
+  'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 
+  'u', 'v', 'w', 'x', 'y', 'z'
+];
+
+const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+function correctPassword(pass) {
+    const hasUpper = [...pass].some(char => upperWord.includes(char));
+    const hasLower = [...pass].some(char => lowerWord.includes(char));
+    const hasNumber = [...pass].some(char => numbers.includes(char));
+
+    if (pass.length > 0 && !hasUpper) {
+        return <p className="alert">A senha deve ter uma letra maiúscula.</p>;
+    }
+
+    if (pass.length > 0 && !hasLower) {
+        return <p className="alert">A senha deve ter uma letra minúscula.</p>;
+    }
+
+    if (pass.length > 0 && !hasNumber) {
+        return <p className="alert">A senha deve ter um número.</p>;
+    }
+
+    if (pass.length > 0 && pass.length < 8) {
+        return <p className="alert">A senha deve ter pelo menos 8 caracteres.</p>;
+    }
+
+    return null;
+}
+
+function matchingPasswords(pass1, pass2) {
+    if (pass1.length > 0 && pass2.length > 0 && pass1 !== pass2) {
+        return <p className="alert">As duas senhas devem ser iguais</p>;
+    }
+}
+
 function RegisterPage() {
     const [username, setUsername] = useState('')
     const [email, setEmail] = useState('')
     const [password1, setPassword1] = useState('')
     const [password2, setPassword2] = useState('')
     
-    function handleSubmit(event) {
-        event.preventDefault()
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        if (
+            username.length === 0 ||
+            email.length === 0 ||
+            password1.length === 0 ||
+            password2.length === 0
+        ){return;}
+
+        const response = await fetch(
+            "http://localhost:8000/auth/user_registration/",
+            {
+                method: "POST",
+
+                headers: {
+                    "Content-Type": "application/json"
+                },
+
+                body: JSON.stringify({
+                    username: username,
+                    email: email,
+                    password1: password1,
+                    password2: password2
+                })
+            }
+        );
+
+        const data = await response.json();
+
+        console.log(data);
     }
 
-        return (
+    return (
         <div className='main'>
             <h1>Registrar Usuario</h1>
             <form className='form'onSubmit={handleSubmit}>
@@ -50,6 +123,9 @@ function RegisterPage() {
                         onChange={(event) => setPassword1(event.target.value)}
                         placeholder="Digite a senha"
                     />
+
+                    {correctPassword(password1)}
+
                 </div>
 
                 <div className='field'>
@@ -62,9 +138,19 @@ function RegisterPage() {
                         onChange={(event) => setPassword2(event.target.value)}
                         placeholder="Confirme a senha"
                     />
+
+                    {matchingPasswords(password1, password2)}
                 </div>
 
-                <button type="submit">
+                <button 
+                    type="submit"
+                    disabled={
+                    username.length === 0 ||
+                    email.length === 0 ||
+                    password1.length === 0 ||
+                    password2.length === 0
+                    }
+                >
                     Confirmar
                 </button>
 
